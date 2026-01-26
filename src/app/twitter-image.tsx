442212be14b6d7494cedef8787af91e4838e061f
Mitesh Shah <Mr.Miteshah@gmail.com>
2026-01-26 14:18:15 +0530

    OpenGraph

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,7 +1,5 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
-
 export const alt = "AnsiPress - Ansible-Powered Hosting Automation";
 export const size = {
   width: 1200,
